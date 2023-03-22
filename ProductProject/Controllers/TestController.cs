c458751e61806bf932b595c800758ef09fd4ac5a
kkele <kkele@DESKTOP-MM84V2T>
2023-03-23 01:28:46 +0300

    Add project files.

---
+++ ProductProject/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductProject.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserList()
+        {
+            return View();
+        }
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+        public ActionResult ToDoList()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProductProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductProject.MvcApplication" Language="C#" %>
